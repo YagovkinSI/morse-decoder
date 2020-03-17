93c2007159196cd702df7b4c9d870d091930f35e
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-03-17 18:15:01 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var codes = expr.split("");
+    var result = "";
+    for (var i = 0; i < codes.length; i += 10) {
+        if (codes[i] == "*") {
+            result += " ";
+            continue;
+        }
+        var value = ""; 
+        for (var j = 0; j < 10; j += 2) {
+            var signal = codes[i + j] + codes[i + j + 1];
+            switch (signal) {
+                case "00":
+                    break;
+                case "10":
+                    value += ".";
+                    break;
+                case "11":
+                    value += "-";
+                    break;
+            }
+        }
+        result += MORSE_TABLE[value];
+    }
+    return result;
 }
 
 module.exports = {
